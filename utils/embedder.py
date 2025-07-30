bac25eb4409dc323e60fa14ece02dfb2a503cc01
NIK Nerkar <iamp1.nik@outlook.com>
2025-07-30 15:40:54 +0530

    updated : embedder.py -> used open_clip from open_ai_clip

--- utils/embedder.py
+++ utils/embedder.py
@@ -1,20 +1,28 @@
-from sentence_transformers import SentenceTransformer
+
 import torch
 from PIL import Image
-import clip
+import open_clip
+from sentence_transformers import SentenceTransformer
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-text_model = SentenceTransformer('all-MiniLM-L6-v2')
-clip_model, clip_preprocess = clip.load("ViT-B/32", device=device)
+# Text model
+text_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# CLIP model
+clip_model, _, preprocess = open_clip.create_model_and_transforms(
+    "ViT-B-32", pretrained="laion2b_s34b_b79k"
+)
+clip_model.to(device)
 
 def embed_text(text):
     return text_model.encode(text)
 
 def embed_image(image_path):
-    image = clip_preprocess(Image.open(image_path)).unsqueeze(0).to(device)
+    image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
     with torch.no_grad():
-        return clip_model.encode_image(image).cpu().numpy()[0]
+        image_features = clip_model.encode_image(image)
+    return image_features.cpu().numpy()[0]
 
 def embed_query(query):
-    return embed_text(query)  # for now, just text
+    return embed_text(query)
